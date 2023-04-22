e85e0ad2d7772f7b65647d0a4f25de2d6c04169f
Saul Almanzar <almanzarsaul@icloud.com>
2023-04-21 23:12:46 -0400

    Spam protection for same queries

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ const selectedColorEl = document.getElementById("color-selector");
 const selectedModeEl = document.getElementById("modes");
 const colorsEl = document.getElementsByClassName("color");
 
+let lastSelectedMode =
+  selectedModeEl[Math.floor(Math.random() * selectedModeEl.options.length)]
+    .value;
+let lastSelectedColor = Math.floor(Math.random() * 16777215).toString(16);
+
 // selects random mode
-selectedModeEl.value =
-  selectedModeEl[
-    Math.floor(Math.random() * selectedModeEl.options.length)
-  ].value;
+selectedModeEl.value = lastSelectedMode;
 
 // selects random color
-selectedColorEl.value = "#" + Math.floor(Math.random() * 16777215).toString(16);
+selectedColorEl.value = "#" + lastSelectedColor;
+
+// get color scheme
 fetch(
-  `https://www.thecolorapi.com/scheme?hex=${selectedColorEl.value.substring(
-    1
-  )}&mode=${selectedModeEl.value}&count=5`
+  `https://www.thecolorapi.com/scheme?hex=${lastSelectedColor}&mode=${lastSelectedMode}&count=5`
 )
   .then((response) => response.json())
   .then((data) => {
@@ -26,13 +28,22 @@ fetch(
 getColorBtn.addEventListener("click", function () {
   const selectedColor = selectedColorEl.value.substring(1); // Removes hashtag from front of color.
   const selectedMode = selectedModeEl.value;
-  fetch(
-    `https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${selectedMode}&count=5`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      data.colors.forEach((color, index) => {
-        colorsEl[index].style.backgroundColor = color.hex.value;
+
+  // prevents spamming API with same query
+  if (
+    selectedColor !== lastSelectedColor ||
+    selectedMode !== lastSelectedMode
+  ) {
+    lastSelectedColor = selectedColor;
+    lastSelectedMode = selectedMode;
+    fetch(
+      `https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${selectedMode}&count=5`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        data.colors.forEach((color, index) => {
+          colorsEl[index].style.backgroundColor = color.hex.value;
+        });
       });
-    });
+  }
 });
